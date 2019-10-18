377cec4952859d0a8cb95c7a623f478c92791d8b
Oxmel <oxm3l@hotmail.com>
2019-10-18 23:44:00 +0100

    special home folder for root

--- import-config.sh
+++ import-config.sh
@@ -4,31 +4,34 @@
 # copies them in the home of the current user.
 
 
-# Checking if git is installed
+# Check if git is installed
 if ! command -v git &> /dev/null; then
     echo "This script requires git"
     exit 1
 fi
 
 repo_url="https://github.com/Oxmel/dotfiles.git"
-cur_user=$(whoami)
-
-# Path aliases
-home="/home/$cur_user"
 dotfiles="/tmp/dotfiles"
 
-echo "Importing config files and folders"
+# Construct path to the user's home
+if [[ $EUID -ne 0 ]]; then
+    home="/home/$(whoami)"
+else
+    home="/root"
+fi
+
+echo "Cloning config files from $repo_url"
 git clone --quiet $repo_url $dotfiles
 
-# Backing up the original bashrc
+# Back up the original bashrc
 mv $home/.bashrc $home/bashrc.bak
 
-# Copying config files to the user's home
+echo "Copying config files to $home"
 cp $dotfiles/bashrc $home/.bashrc
 cp $dotfiles/vimrc $home/.vimrc
 cp -r $dotfiles/vim $home/.vim
 
-# Cleaning up
+# Clean up
 rm -rf $dotfiles
 
 echo "Done"
